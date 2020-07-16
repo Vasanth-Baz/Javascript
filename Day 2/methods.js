2ab46e8cce9d63ddb664069599087e24a5e71f1c
Vasanth-Baz <68318749+Vasanth-Baz@users.noreply.github.com>
2020-07-16 21:53:27 +0530

    Add files via upload

---
+++ Day 2/methods.js
@@ -0,0 +1,37 @@
+console.log("Methods");
+
+let senten = "Today weather is Awesome!!! Lets go for a cup of coffee hey."; 
+console.log(senten);
+
+console.log(prompt("Enter the number you like",0));
+
+console.log(Array(senten))
+console.log(senten.startsWith("Today"));
+console.log(senten.endsWith("hey."));
+// let vay = senten.trim();
+// console.log(vay)
+
+console.log(senten.split('e'));
+console.log(senten.charAt(13))
+console.log(senten.sub());
+console.log(senten.substring(6,13));
+console.log(senten.slice(6));
+let result = senten.replace("Today" | "Yesterday");
+console.log(result);
+// console.log(senten.toLocaleLowerCase(senten["Awesome"]));
+console.log(senten.toLowerCase());
+console.log(senten.toUpperCase());
+// console.log(senten.trim());
+
+arr = ["Sachin", "Dhoni", "Virat", "Raina"]
+arr1 = ["Ronoldo", "Messsi", "Neymer"]
+console.log(arr, arr1)
+
+console.log(arr.reverse());
+console.log(arr[3]);
+console.log(arr.slice(1,3));
+// console.log(arr.replace("Sachin" | "Bradman"));
+
+
+console.log(arr.concat(arr1));
+console.log(arr1.concat(arr));
